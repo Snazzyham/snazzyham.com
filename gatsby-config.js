3d95ebd654b65e535793fbdc52d7de9d0d377eec
Soham Adwani <sohamthenerd@gmail.com>
2018-07-02 13:34:22 +0700

    added SCSS and switched to Preact

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,9 @@ module.exports = {
   siteMetadata: {
     title: "Soham Adwani - Web App Developer based in Indonesia"
   },
-  plugins: ["gatsby-plugin-react-helmet"]
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-preact",
+    "gatsby-plugin-sass"
+  ]
 };
