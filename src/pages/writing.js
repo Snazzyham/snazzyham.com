d1e98f26a1797f699fff514aaee2ccda5672c7df
Soham Adwani <sohamthenerd@gmail.com>
2018-07-03 12:54:18 +0700

    designed blog page

--- src/pages/writing.js
+++ src/pages/writing.js
@@ -1,21 +1,26 @@
 import React from "react";
 import Link from "gatsby-link";
-
+import Terminal from "../images/terminal.svg";
+import Pencil from "../images/pencil.svg";
 const PostContainer = props =>
-  <div key={props.key}>
-    <Link to={`/${props.path}`} class="link">
-      <h1 class="dim">
+  <div key={props.key} class="w-60-ns center ph2 pv3">
+    <Link to={`/${props.path}`} class="link black-80">
+      <h1 class="dim mb1">
         {props.title}
       </h1>
     </Link>
-    <h3>
+    <h3 class="mt0 black-60 f5 fw3">
       {props.date}
     </h3>
-    <p dangerouslySetInnerHTML={{ __html: props.excerpt }} />
+    <p class="black-70" dangerouslySetInnerHTML={{ __html: props.excerpt }} />
   </div>;
 
 const WritingPage = ({ data }) =>
   <div class="fl w-100">
+    <div class="fl w-100 ph5 tc mb5-ns mb3">
+      <img src={Terminal} alt="Code" width={150} class="mr4-ns" />
+      <img src={Pencil} alt="blog" width={135} />
+    </div>
     {data.allMarkdownRemark.edges.map(x =>
       <PostContainer
         key={x.node.id}
