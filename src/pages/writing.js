88f9af20218991f34c2c7f611fb57e9fd503e581
Soham Adwani <sohamthenerd@gmail.com>
2018-07-03 13:28:37 +0700

    added dayjs for date, as well as sorting graphql results by date

--- src/pages/writing.js
+++ src/pages/writing.js
@@ -2,6 +2,8 @@ import React from "react";
 import Link from "gatsby-link";
 import Terminal from "../images/terminal.svg";
 import Pencil from "../images/pencil.svg";
+import dayjs from "dayjs";
+
 const PostContainer = props =>
   <div key={props.key} class="w-60-ns center ph2 pv3">
     <Link to={`/${props.path}`} class="link black-80">
@@ -10,7 +12,7 @@ const PostContainer = props =>
       </h1>
     </Link>
     <h3 class="mt0 black-60 f5 fw3">
-      {props.date}
+      {dayjs(props.date).format("MMMM YYYY")}
     </h3>
     <p class="black-70" dangerouslySetInnerHTML={{ __html: props.excerpt }} />
   </div>;
@@ -34,7 +36,9 @@ const WritingPage = ({ data }) =>
 
 export const pageQuery = graphql`
   query IndexQuery {
-    allMarkdownRemark {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date_published], order: DESC }
+    ) {
       edges {
         node {
           id
