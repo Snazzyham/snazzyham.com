8d0dea1a38ca6de34a3de7da27fba673f67e80e6
Soham Adwani <sohamthenerd@gmail.com>
2018-07-03 15:38:25 +0700

    added four-oh-four

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from "react";
 
-const NotFoundPage = () => (
-  <div>
+const NotFoundPage = () =>
+  <div class="fl w-100 ph5">
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
-)
+    <iframe
+      src="https://giphy.com/embed/L3pfVwbsJbrk4"
+      width="480"
+      height="270"
+      frameBorder="0"
+      class="giphy-embed"
+      allowFullScreen
+    />
+    <p>
+      <a href="https://giphy.com/gifs/disney-star-wars-darth-vader-L3pfVwbsJbrk4">
+        via GIPHY
+      </a>
+    </p>
+  </div>;
 
-export default NotFoundPage
+export default NotFoundPage;
