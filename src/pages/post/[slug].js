4c80c8ec42fb854ee17e844fbbfad65b9a06d1fb
Soham Adwani <sohamthenerd@gmail.com>
2020-03-27 19:20:45 +0700

    added blog post page

---
+++ src/pages/post/[slug].js
@@ -0,0 +1,44 @@
+import { getData } from "../../helpers";
+import { RichText } from "prismic-reactjs";
+import Layout from "../../layouts";
+
+export const getStaticPaths = async () => {
+  const res = await getData("document.type", "blog_post");
+  let paths = res.data.results.map(x => {
+    return {
+      params: {
+        slug: x.uid
+      }
+    };
+  });
+
+  return {
+    paths,
+    fallback: false
+  };
+};
+
+export const getStaticProps = async ctx => {
+  const res = await getData("my.blog_post.uid", ctx.params.slug);
+  let data = res.data.results[0].data;
+
+  return {
+    props: { data }
+  };
+};
+
+const BlogPost = ({ data }) => {
+  return (
+    <Layout>
+      <div className="blogpost">
+        <article className="post">
+          <h1>{RichText.asText(data.title)}</h1>
+          <p className="date">{data.date}</p>
+          <RichText render={data.body} />
+        </article>
+      </div>
+    </Layout>
+  );
+};
+
+export default BlogPost;
