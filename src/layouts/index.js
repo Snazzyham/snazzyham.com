11b6650c189a3adf447504c6131be606c266c7db
Soham Adwani <sohamthenerd@gmail.com>
2020-03-28 20:37:50 +0700

    added animation components

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,16 +1,28 @@
 import { useState } from "react";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
+import { motion } from "framer-motion";
+
+const variants = {
+  hide: { opacity: 0 },
+  show: { opacity: 1 }
+};
 
 const Layout = props => {
   const [theme, setTheme] = useState("isDark");
 
   return (
-    <div className={`layout-wrapper ${theme}`}>
+    <motion.div
+      className={`layout-wrapper ${theme}`}
+      initial={"hide"}
+      animate={"show"}
+      exit={"hide"}
+      variants={variants}
+    >
       {props.showNav !== false && <Navbar />}
       <main className="main">{props.children}</main>
       <Footer />
-    </div>
+    </motion.div>
   );
 };
 
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,18 @@
 import Head from "next/head";
+import { AnimatePresence } from "framer-motion";
 
 import "../styles/normalize.css";
 import "../styles/main.scss";
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps, router }) {
   return (
     <>
       <Head>
         <title>Soham Adwani - Web App Developer based in Indonesia</title>
       </Head>
-      <Component {...pageProps} />
+      <AnimatePresence exitBeforeEnter>
+        <Component {...pageProps} key={router.route} />
+      </AnimatePresence>
     </>
   );
 }
