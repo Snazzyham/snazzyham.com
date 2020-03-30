5e9cb7e11703e3f58e0deab532cd762d24b6a263
Soham Adwani <sohamthenerd@gmail.com>
2020-03-30 21:26:35 +0700

    added GA

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,15 @@
 import Head from "next/head";
 import { AnimatePresence } from "framer-motion";
+import { useEffect } from "react";
+import ReactGA from "react-ga";
 
 import "../styles/normalize.css";
 import "../styles/main.scss";
 
 export default function MyApp({ Component, pageProps, router }) {
+  useEffect(() => {
+    ReactGA.initialize("UA-84133610-1");
+  }, []);
   return (
     <>
       <Head>
